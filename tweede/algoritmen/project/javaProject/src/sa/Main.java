0e41e1ebe1252b16e58a67782b22577f1846b7fb
B <strenuous@protonmail.com>
2017-11-16 14:50:46 +0100

    papyrus

---
+++ tweede/algoritmen/project/javaProject/src/sa/Main.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package sa;
+
+/**
+ * @author b
+ * 
+ * @version 1
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
